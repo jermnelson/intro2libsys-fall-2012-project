38489867e158fe5f75cbf6b4a831511d76628b9b
Jeremy Nelson <jermnelson@gmail.com>
2012-10-23 17:18:55 -0600

    Added RELS-EXT template and started working on Ingest

--- src/Ingest.py
+++ src/Ingest.py
@@ -1,11 +1,29 @@
 __author__ = "Chris Coughlan, Brian Shoemaker, Katharine Hales"
 
-import common
-from eulfedora.models import DigitalObject,FileDatastream,XmlDatastream,FileDatastreamObject
-
-class ManuscriptDigitalObject(DigitalObject):
-    CONENT_MODELS = [ "info:fedora/" ]
+import common,sys,os,mimetypes
 
 def ingest_manuscript(file_path):
-    manuscript_fedora = common.repository.get_object()
+    # Queries repository and gets the next available pid in the coccc
+    # namespace
+    new_pid = common.repository.get_next_pid(namespace="coccc")
+    # Opens up the mods.xml from the directory
+    mods = etree.XML(open(os.path.join(file_path,"mods.xml"),'rb'))
+    # Adds MODS datastream to the new object
+    common.repository.api.addDatastream(pid=new_pid,
+                                        dsID="MODS",
+                                        dsLabel="MODS",
+                                        mimeType="application/rdf+xml",
+                                        content=etree.tostring(mods))
+    # create a file directory walker to find image files in the directory
+    manuscript_files = next(os.walk(file_path))[1]
+    for filename in manuscript_files:
+        file_root,file_ext = os.path.splitext(filename)[1]
+        if [".png",".jpg",".gif"].count(file_Ext) > 0:
+            common.repository.api.addDatastream(pid=new_pid,
+                                                dsID=file_root,
+                                                dsLabel=file_root,
+                                                mimeType=,
+                                                content=)
+    # finally, add RELS-EXT datastream
+    
     
