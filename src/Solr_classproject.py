c13f0203c8ee85a5bbd80327aebc147be325e3ea
clmattoon <cldinneen@gmail.com>
2012-11-05 21:08:27 +0000

    Added function to return object title from MODS

--- src/Solr_classproject.py
+++ src/Solr_classproject.py
@@ -5,48 +5,28 @@ import xml.etree.ElementTree as etree
 
 FIELDNAMES = [
     'access', # Should have a constant value of "Online"
-    'audience',
     'author', #namePart
-    'bib_num',
-    'callnum',
-    'callnumlayerone',
     'collection', #! Incorrect
     'contents', # Should be all of the text of a transcription (if present)
-    'corporate_name',
-    'ctrl_num',
-    'description',
-    'era',
     'format', #! Incorrect this should not be a subject
     'full_title', #title
-    'full_lc_subject',
-    'genre',
-    'holdings',
     'id', #system generated, should be the PID
-    'imprint',
-    'isbn',
-    'issn',
-    'item_ids',
-    'language',
-    'language_dubbed', 
-    'language_subtitles',
-    'lc_firstletter',
     'location', #! Incorrect, this should be a constant of dacc
-    'marc_record',
-    'oclc_num',
     'notes', #! Incorrect, only include public access notes (not record notes), abstract
     'personal_name', #namePart
-    'place',
-    'publisher',
-    'publisher_location',
-    'pubyear',
-    'series',
-    'summary',
+    'summary', # abstract
     'title', # title
-    'title_sort',
     'topic', #subject
-    'upc',
     'url', # Should be the URL in the location 
 ]
+
+def get_title(mods):
+    """
+    Function takes the objects MODS and extracts and returns the text of the title.
+    """
+    title = mods.find("{{{0}}}name/{{{0}}}title".format(common.MODS_NS))
+    return title.text
+
 
 def get_author(mods):
     """
