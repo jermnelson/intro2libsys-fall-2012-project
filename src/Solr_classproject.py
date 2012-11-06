eff93c4b4a0951a0509361cd3be76f1460c3a7d3
clmattoon <cldinneen@gmail.com>
2012-11-06 00:45:47 +0000

    Updated code for fieldname functions

--- src/Solr_classproject.py
+++ src/Solr_classproject.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as etree
 FIELDNAMES = [
     'access', # Should have a constant value of "Online"
     'author', #namePart
-    'collection', #! Incorrect
+    'bib_id', # Pid
     'contents', # Should be all of the text of a transcription (if present)
     'format', #! Incorrect this should not be a subject
     'full_title', #title
@@ -24,9 +24,18 @@ def get_title(mods):
     """
     Function takes the objects MODS and extracts and returns the text of the title.
     """
-    title = mods.find("{{{0}}}name/{{{0}}}title".format(common.MODS_NS))
+    title = mods.find("{{{0}}}titleInfo/{{{0}}}title".format(common.MODS_NS))
     return title.text
 
+def get_topics(mods):
+    """
+    Function takes the objects MODS and returns the text of the topics.
+    """
+    output = []
+    topics = mods.findall("{{{0}}}subject/{{{0}}}topic".format(common.MODS_NS))
+    for topic in topics:
+        output.append(topic.text)
+    return output
 
 def get_author(mods):
     """
@@ -57,4 +66,36 @@ def get_mods(pid):
     # Returns the etree MODS xml object from the raw XML
     return etree.XML(mods_xml)
 
+def get_summary(mods):
+    """
+    Function extracts abstract from MODS and returns text.
+    """
+    summary = mods.find("{{{0}}}abstract".format(common.MODS_NS))
+    return summary.text
 
+def get_url(mods):
+    """
+    Function extracts URL location from MODS and returns text.
+    """
+    url = mods.find("{{{0}}}location/{{{0}}}url".format(common.MODS_NS))
+    return url.text
+
+def index_manuscript(pid):
+    """
+    Function takes PID, extracts MODS, creates Solr document and attempts to ingest into Solr.
+    """
+
+    mods = get_mods(pid)
+    Solr_doc = {'access':'Online',
+                'bib_id':pid,
+                'format':'Manuscript',
+                'location':'dacc',
+                'id':pid}
+    Solr_doc['author'] = get_author(mods)
+    Solr_doc['title'] = get_title(mods)
+    Solr_doc['full_title'] = Solr_doc['title']
+    Solr_doc['topic'] = get_topics(mods)
+    Solr_doc['summary'] = get_summary(mods)
+    Solr_doc['notes'] = Solr_doc['summary']
+    Solr_doc['personal_name'] = Solr_doc['author']
+    Solr_doc['url'] = get_url(mods)
